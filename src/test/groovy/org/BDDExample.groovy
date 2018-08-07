4836d46d15748565481be34f33658c881a021ac6
PDC2B-Training.PDC2B <PDC2B-Training.PDC2B@P2B-6BRCNK2.dir.svc.accenture.com>
2018-08-07 14:49:12 +0530

    Commit

---
+++ src/test/groovy/org/BDDExample.groovy
@@ -0,0 +1,18 @@
+package org
+
+import spock.lang.Specification
+
+class BDDExample extends Specification{
+	def"Scenario: multiply"()
+	{
+		given:"An integer with value 10"
+		def i = 10
+		
+		when: "This number is multiplies with 2"
+		i = i*2
+		
+		then:"The final value is 20"
+		assert 20 ==i
+	}
+
+}
